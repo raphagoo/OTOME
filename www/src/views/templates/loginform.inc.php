0c4a7c53c76809ec1c9612aa89f6e04a532fe660
raphagoo <raphael.agostini@ynov.com>
2017-11-29 17:23:16 +0100

    'connexion'

--- www/src/views/templates/loginform.inc.php
+++ www/src/views/templates/loginform.inc.php
@@ -1,49 +1,6 @@
-<img id="speakerheader" src="img/speaker.png">
-<a id="inscription" class="" href="<?php echo $_SERVER['PHP_SELF'].'?action=SignUpForm';?>">Inscription</a>
-<a id="connection" class="" href="">Connexion</a>
-
-
-<!--
 <form class="navbar-form pull-right" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>?action=Login" >
-   <div id="inscription">
-    <a class="" href="<?php echo $_SERVER['PHP_SELF'].'?action=SignUpForm';?>">Inscription</a>
-   </div>
-    <div id="fondco">
-      <div id="titreco">
-        <h2>Connexion</h2>
-      </div>
-
-
-  	<input id="identco" class="" placeholder="Pseudo" name="nickname" type="text" />
-  	<input id="mdpco" class="" placeholder="Mot de passe" name="password"	type="password" />
-  	<input id="bouton" class="" name="connexionConnexion" type="submit" value="Connexion" />
-  </form>
--->
-
-<!--
-<div id="fondco">
-    <div id="titreco">
-        <h2>Connexion :</h2>
-    </div>
-
-
-    <form action="" method="POST">
-
-        <div class="form-group" id="identco">
-            <label for=""><h2>Identifiant</h2></label>
-            <input type="text" name="username" class="form-control"/>
-        </div>
-
-        <div class="form-group" id="mdpco">
-            <label for=""><h2>Mot de passe</h2></label>
-            <input type="text" name="email" class="form-control "/>
-        </div>
-        <a href="#" id="mdpoublie">Mot de passe oublié ?</a>
-        <button type="submit" class="btn btn-primary" id="bouton">Se connecter</button>
-    </form>
-    <div id="rappelco">
-        <label for="certif">Se souvenir de moi ?</label>
-        <input type="checkbox" id="certif" name="certif">
-    </div>
-</div>
--->
+    <a class="btn btn-danger" href="<?php echo $_SERVER['PHP_SELF'].'?action=SignUpForm';?>">Inscription</a>
+    <input class="span2" placeholder="Pseudo" name="nickname" type="text" />
+    <input class="span2" placeholder="Mot de passe" name="password"	type="password" />
+    <input class="btn" name="connexionConnexion" type="submit" value="Connexion" />
+</form>
