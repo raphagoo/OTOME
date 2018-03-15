057db5860e57596210db3aaa793cb6c06c4756a5
raphagoo <33906573+raphagoo@users.noreply.github.com>
2018-03-15 13:51:16 +0100

    Add files via upload
    Choix complet

--- jeu.php
+++ jeu.php
@@ -1,56 +1,64 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
   <head>
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Startup</title>
-    <link rel="stylesheet" href="choix.css">
+    <link rel="stylesheet" href="choi.css">
     <script type="text/javascript"></script>
     <script src="jquery.js"></script>
 
   </head>
   <body>
-    <div class="choix">
-      <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-        <input type="radio" name="choix" value="<?php if (isset($_POST['choix'])){echo ($_POST['choix'].'1');}else{echo '1';}?>"> Choix 1<br>
-        <input type="radio" name="choix" value="<?php if (isset($_POST['choix'])){echo ($_POST['choix'].'2');}else{echo '2';}?>">Choix 2<br>
-        <input type="submit" name="Valider" value="Valider">
-      </form>
-    </div>
+  <?php require('jeuphp.php'); ?>
+  <?php
+  if(isset($_POST['choix'])){
+      $id = $_POST['choix'];
+  }
+  else{
+      $id = 11;
+  }
+  $id2 = $id+1;
+
+
+  $id = str_replace(0,1,$id);
+  $dernierchar =  substr($id2,-1, 1);
+  $id2 = str_replace(0,1,$id2);
+  if ($dernierchar == 1) {
+      $dernierchar = $dernierchar + 1;
+  }
+  $strlenid2 = strlen($id2)-1;
+  $choixx = substr($id2,0,$strlenid2);
+  $choix2 = $choixx.$dernierchar;
+  $choix = $id;
+  $id = intval($id);
+  $id2 = intval($id2);
+  ?>
     <div id="conteneur">
       <div class="img" id="img1"></div>
+      <div id="personnage"></div>
       <div id="boitedialogue">
+          <div class="choix">
+              <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+                  <input type="radio" name="choix" value="<?php if (isset($id)){echo ($id.'0');}else{echo '1';}?>"><?php avoirinfo("choix","choix",$choix)?><br> <!-- Permet de récupérer le dialogue selon l'ID dans la BDD -->
+                  <input type="radio" name="choix" value="<?php if (isset($id)){echo ($id.'1');}else{echo '2';}?>"><?php avoirinfo("choix","choix",$choix2)?><br> <!-- Même chose -->
+                  <input type="submit" name="Valider" value="Valider">
+              </form>
+          </div>
+          <div id="bouton"><button id="boutonchoix">Place au choix</button></div>
         <div id="dialogue"></div>
         <div id="test">
       </div>
     </div>
   </div>
-    <button id="boutonchoix">Place au choix</button>
-    <?php require('jeuphp.php'); ?>
-<?php echo
-    "<script>
-      var compteur = 0;
-      var choixprec = 0;
-      function click_choix(){\"";
-        echo "\";
-        document.getElementById('boutonchoix').style.visibility='visible';
-        compteur = compteur+1;
-        $('.choix').hide();
-        $('.img').show();
-        $('#conteneur').show();\"";
-            echo "\"
-            document.getElementById('img1').innerHTML=\"";
-            avoirinfo("image", "fond", $_POST['choix']);echo "\";
-            document.getElementById('dialogue').innerHTML=\"";
-            avoirinfo("dialogue","dialogue",$_POST['choix']);
-    echo "\";
-      $('#boutonchoix').click(function(){
-        $('#conteneur').hide();
-        $('.img').hide();
-        $('.choix').show();
-        document.getElementById('boutonchoix').style.visibility='hidden';
-      });
-    }
-click_choix();
-</script>"; ?>
+<?php
+
+            echo "<script>\"
+            document.getElementById('img1').innerHTML=\"";  // On exécute la fonction avoirinfo de jeuphp.php avec l'id set dans le formulaire (à changer)
+            avoirinfo("image", "fond", 1);echo "<script>\";
+            document.getElementById('dialogue').innerHTML=\"";  // On éxécute la même fonction pour le dialogue d'abord le premier en id 11 puis le suivant (boucle à faire pour que la fonction soit récursive
+            avoirinfo("dialogue","dialogue",$id);
+            avoirinfo("dialogue","dialogue",$id2);echo "<script>\"
+            document.getElementById('personnage').innerHTML=\""; // Appel à la fonction pour récupérer les images des personnages
+            avoirinfo("image", "personnages", 1); ?>
   </body>
 </html>
--- jeuphp.php
+++ jeuphp.php
@@ -2,11 +2,11 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="choi.css">
+    <script src="jquery.js"></script>
 </head>
 <body>
-
 <?php
-
 function avoirinfo($param1, $param2, $param3)
 {
     $servername = "localhost";
@@ -14,14 +14,40 @@ function avoirinfo($param1, $param2, $param3)
     $password = "";
     $dbname = "startup";
     try {
-        $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        // set the PDO error mode to exception
+        $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')); // Connexion + UTF-8 //
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $infos = $bdd->query("SELECT $param1 FROM $param2 WHERE id=$param3");
+        $infos = $bdd->query("SELECT $param1 FROM $param2 WHERE id=$param3"); // SELECT en language SQL selon les parametres
         $infos->setFetchMode(PDO::FETCH_OBJ);
-
-        if ($param1 == 'image'){
+        if($param1 == 'dialogue'){ // Si on select du dialogue
+          $infofetch = $infos->fetch(PDO::FETCH_NUM); // Récup l'info sous forme de dialogue
+          $info = implode("",$infofetch);
+          $infoexp = explode(".",$info); // Permet d'obtenir un tableau dont chaque variable contient une phrase
+          $longueur = count($infoexp); // Compte la longueur
+          ?>
+          <script>
+              var longueur = <?php echo "$longueur" ?>;
+            var infoexp = new Array;
+            infoexp = <?php echo json_encode($infoexp); ?>; // Récupère le tableau en PHP et le convertit en JS
+            var i = 1;
+          document.getElementById('dialogue').innerHTML=infoexp[0]; // Affiche la première phrase du dialogue
+            $('#boitedialogue').click(function(){ // Clic sur la boite de dialogue
+              if(i < longueur-1){ // Tant que i est plus petit que la longueur du tableau
+              document.getElementById('dialogue').innerHTML=infoexp[i]; // Afficher la phrase suivante du dialogue
+              i = i+1;
+            }
+            else{
+              $('#boutonchoix').show();
+              $('#dialogue').empty();
+            }
+            });
+              $('#boutonchoix').click(function() { // Au clic sur le bouton on fait apparaître les choix.
+                  $('#boutonchoix').hide();
+                  $('.choix').show();
+              });
+        </script>
+        <?php
+        }
+        else if ($param1 == 'image'){ // Si le SELECT est sur une image
 
             $infos->bindColumn(1, $image, PDO::PARAM_LOB);
             $infos->fetch(PDO::FETCH_BOUND);
@@ -30,7 +56,7 @@ function avoirinfo($param1, $param2, $param3)
             print_r($image); echo '>';
         }
         else {
-            while ($info = $infos->fetch()) {
+            while ($info = $infos->fetch()) { // Si le select est sur autre chose qu'une image ou du dialogue
                 echo  $info->$param1 . '<br>';
             }
         }
