217d9feb74fafd868265b69dad4f5419115c5f47
raphagoo <33906573+raphagoo@users.noreply.github.com>
2018-04-04 16:46:41 +0200

    Add files via upload

--- jeu.php
+++ jeu.php
@@ -3,24 +3,22 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Startup</title>
-    <link rel="stylesheet" href="choi.css">
+    <link rel="stylesheet" href="choix.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <script type="text/javascript"></script>
-    <script src="jquery.js"></script>
-
+    <script src="jquery.js" type="text/javascript"></script>
 </head>
-<body>
 <?php require('jeuphp.php'); ?>
+<body>
 <div id="conteneur">
     <div id="boiteimg">
+        <div id="noir"></div>
         <div id="img1"></div>
-        <img id="personnage">
     </div>
 
     <div id="boitedialogue">
         <div class="choix">
             <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-                <input type="radio" name="choix" value="<?php if (isset($id)){echo ($id.'0');}else{echo '1';}?>"><?php avoirinfo("choix","choix",$choix)?><br> <!-- Permet de récupérer le dialogue selon l'ID dans la BDD -->
+                <input type="radio" name="choix" value="<?php if (isset($id)){echo ($id.'0');}else{echo '1';}?>"><?php avoirinfo("choix","choix",$choix)?> <!-- Permet de récupérer le dialogue selon l'ID dans la BDD -->
                 <input type="radio" name="choix" value="<?php if (isset($id)){echo ($id.'1');}else{echo '2';}?>"><?php avoirinfo("choix","choix",$choix2)?><br> <!-- Même chose -->
                 <input type="submit" name="Valider" value="Valider">
             </form>
@@ -30,6 +28,7 @@
         <div id="test">
         </div>
     </div>
+</div>
     <?php
 echo "<script>";echo "
             document.getElementById('dialogue').innerHTML=";  // On éxécute la même fonction pour le dialogue d'abord le premier en id 11 puis le suivant (boucle à faire pour que la fonction soit récursive
