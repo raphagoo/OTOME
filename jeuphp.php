c1e89cae009341c0a6341eabe8efce5dfd9c8f12
raphagoo <33906573+raphagoo@users.noreply.github.com>
2018-04-04 16:45:53 +0200

    ajout slides

--- jeuphp.php
+++ jeuphp.php
@@ -4,10 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="choi.css">
+    <link rel="stylesheet" href="choix.css">
     <script src="jquery.js"></script>
 </head>
 <body>
+<script type="text/javascript">
+    function changementlieu(param1){
+            $('#noir').delay(1000).fadeIn(1000);
+            $('#boitedialogue').delay(1000).fadeOut(2500);
+            $('#img1').delay(1000).fadeOut(2500)
+                .delay(2000)
+                .queue(function(n) {
+                    document.getElementById('dialogue').innerHTML=infoexp[i];
+                    i = i+1;
+                    $('#img1').html(param1);
+                    n();
+                }).fadeIn(500);
+            $('#boitedialogue').delay(2500).fadeIn(500);
+            $('#noir').delay(2000).fadeOut(2000);
+    }
+</script>
 <?php
 if(isset($_POST['choix'])){
     $id = $_POST['choix'];
@@ -51,7 +67,6 @@ function avoirinfo($param1, $param2, $param3)
                 var longueur = <?php echo "$longueur" ?>;
                 var infoexp = new Array;
                 var id2 = <?php echo $id2 ?>;
-
                 infoexp = <?php echo json_encode($infoexp); ?>; // Récupère le tableau en PHP et le convertit en JS
                 var i = 1;
                 if(id2<100){
@@ -69,54 +84,46 @@ function avoirinfo($param1, $param2, $param3)
                     if(i < longueur-1){ // Tant que i est plus petit que la longueur du tableau
                         document.getElementById('dialogue').innerHTML=infoexp[i]; // Afficher la phrase suivante du dialogue
                         i = i+1;
-                        if(id2 < 100 && i<8) {
+                        if(id2 < 100 && i<7) {
                             document.getElementById('img1').innerHTML = "<img src='ig/1_avion.jpg'>";
                        }
-                        else if(id2 <100 && 8<=i && i<=30){
-                            document.getElementById('img1').innerHTML = "<img src='ig/2_4_scene-Tour-Eiffel.jpg'>";
+                        else if(id2 <100 && 7==i){
+                            changementlieu("<img src='ig/2_4_scene-Tour-Eiffel.jpg'>");
                         }
-                        else if(id2 < 100 && 30<i && i<=46){
-                            document.getElementById('img1').innerHTML = "<img src='ig/3_Grenier.jpg'>";
+                        else if(id2 < 100 && 31==i){
+                            changementlieu("<img src='ig/3_Grenier.jpg'>");
                         }
-                        else if(id2 <100 && 46<i && i<=60 ){
-                            document.getElementById('img1').innerHTML = "<img src='ig/2_4_scene-Tour-Eiffel.jpg'>";
+                        else if(id2 <100 && 46==i){
+                            changementlieu("<img src='ig/2_4_scene-Tour-Eiffel.jpg'>");
                         }
-                        else if(id2 > 100 && id2 < 1000 && i<=6 ){
-                            document.getElementById('img1').innerHTML = "<img src='ig/2_4_scene-Tour-Eiffel.jpg'>";
+                        else if(id2 > 100 && id2 < 1000 && i==6){
+                            changementlieu("<img src='ig/5_entree-copy-1.jpg'>");
                         }
-                        else if(id2 > 100 && id2 < 1000 && i>6 && i<=20 ){
-                            document.getElementById('img1').innerHTML = "<img src='ig/5_entree-copy-1.jpg'>";
+                        else if(id2 > 100 && id2 < 1000 && i==20){
+                            changementlieu("");
                         }
-                        else if(id2 > 100 && id2 < 1000 && i>20 && i<=49){
-                            document.getElementById('img1').innerHTML = "";
+                        else if(id2 >100 && id2 <1000 && i==45){
+                            changementlieu("<img src='ig/1_avion.jpg'>");
                         }
-                        else if(id2 >100 && id2 <1000 && i>49 && i<=61){
-                            document.getElementById('img1').innerHTML = "<img src='ig/1_avion.jpg'>";
+                        else if(id2 >100 && id2 <1000 && i==62){
+                           changementlieu("");
                         }
-                        else if(id2 >100 && id2 <1000 && i>61 && i<=95){
-                            document.getElementById('img1').innerHTML = "";
+                        else if (id2 >100 && id2 <1000 && i==96){
+                            changementlieu("<img src='ig/6_salon-tante.jpg'>");
                         }
-                        else if (id2 >100 && id2 <1000 && i>95 && i<=146){
-                            document.getElementById('img1').innerHTML = "<img src='ig/6_salon-tante.jpg'>";
+                        else if(id2 >100 && id2 <1000 && i==147){
+                            changementlieu("<img src='ig/7_entree_bibliotheque.jpg'>");
                         }
-                        else if(id2 >100 && id2 <1000 && i>146){
-                            document.getElementById('img1').innerHTML = "<img src='ig/7_entree_bibliotheque.jpg'>";
+                        else if(id2>1000 && id2<2000 && i==28){
+                            changementlieu("<img src='ig/8_amphitheatre.jpg'>");
                         }
-                        else if(id2>1000 && id2<2000 && i<27){
-                            document.getElementById('img1').innerHTML = "<img src='ig/7_entree_bibliotheque.jpg'>";
+                        else if(id2>1000 && id2<2000 && i==46){
+                            changementlieu("<img src='ig/7_entree_bibliotheque.jpg'>");
                         }
-                        else if(id2>1000 && id2<2000 && i>27 && i<=45){
-                            document.getElementById('img1').innerHTML = "<img src='ig/8_amphitheatre.jpg'>";
+                        else if(id2>1000 && id2<2000 && i==101){
+                            changementlieu("<img src='ig/8_amphitheatre.jpg'>");
                         }
-                        else if(id2>1000 && id2<2000 && i>45 && i<=100){
-                            document.getElementById('img1').innerHTML = "<img src='ig/7_entree_bibliotheque.jpg'>";
-                        }
-                        else if(id2>1000 && id2<2000 && i>100){
-                            document.getElementById('img1').innerHTML = "<img src='ig/8_amphitheatre.jpg'>";
-                        }
-
                     }
-
                     else{
                         $('#boutonchoix').show();
                         $('#dialogue').empty();
@@ -139,8 +146,6 @@ function avoirinfo($param1, $param2, $param3)
                 echo  $info->$param1 . '<br>';
             }
         }
-            ?>
-<?php
     } catch (PDOException $e) {
         print "Erreur! " . "<br>" . $e->getMessage();
         die();
